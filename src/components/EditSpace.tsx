57d0d00af8f76d5a8b881e0c678f7a0f8fda9689
Vishnu c <vishnumalbro124@gmail.com>
2025-07-09 00:40:23 +0530

    avaiable textuers added

--- src/components/EditSpace.tsx
+++ src/components/EditSpace.tsx
@@ -2,19 +2,46 @@ import { Canvas } from '@react-three/fiber'
 import ThreeSpace from './ThreeSpace'
 import { useRef, useState } from 'react'
 
+
 const EditSpace = () => {
   const [color, setColor] = useState<string | undefined>('')
   const inputref = useRef<HTMLInputElement>(null)
+  // const popupRef = useRef<HTMLButtonElement>(null)
+  const [popUpVisible,setPopUpVisible] = useState(false)
 
   const handleColor = () => {
     setColor(inputref.current?.value)
   }
+
+  const texturePopup = () => {
+    setPopUpVisible(previous => !previous);
+
+  }
+
+  const handleImageTexture = (e:React.MouseEvent<HTMLDivElement,MouseEvent>) => {
+    const target = e.target as HTMLImageElement
+    if(target.tagName === "IMG"){
+      console.log(target.src)
+    }
+  }
   return (
     <div className="p-4 flex flex-col items-center min-h-screen bg-gray-100 gap-6">
+      <div className={`${popUpVisible?'block':'hidden'} absolute rounded-2xl p-5 popup z-99 w-[80VW] h-90 bg-amber-100  border-2 border-red-500`}>
+        <button onClick={texturePopup} className={` absolute right-0 p-2 text-3xl text-red-500 font-bold cursor-pointer hover:scale-110`}>X</button>
+        <h1 className='font-mono text-3xl font-semibold underline'>Available textures</h1>
+        <div className='flex justify-start p-5 space-x-5' onClick={handleImageTexture}>
+        <div className='w-40 h-40  bg-black'><img src="/textures/texture1.jpg" className='object-fill h-full hover:scale-110 duration-500' alt="" /></div>
+        <div className='w-40 h-40  bg-black'><img src="/textures/texture2.jpg" className='object-fill h-full hover:scale-110 duration-500' alt="" /></div>
+        <div className='w-40 h-40  bg-black'><img src="/textures/texture3.jpg" className='object-fill h-full hover:scale-110 duration-500' alt="" /></div>
+
+
+        </div>
+      </div>
+
       {/* Canvas Container */}
       <div
         className="
-          bg-amber-200 
+          bg-amber-100 
           w-full 
           max-w-[1200px] 
           h-[60vh] 
@@ -32,8 +59,8 @@ const EditSpace = () => {
         <h2 className="text-xl font-bold text-gray-800 mb-4">Edit Space</h2>
         <Canvas camera={{ position: [0, 2, 5], fov: 20 }}>
           <ThreeSpace color={color} />
-          <ambientLight intensity={0.2} />
-          <pointLight position={[0, 1, 1]} intensity={1.5} />
+          <ambientLight intensity={0.1} />
+          <directionalLight position={[0, 1, 2]} intensity={1.2} />
         </Canvas>
       </div>
 
@@ -43,6 +70,7 @@ const EditSpace = () => {
           ref={inputref}
           type="text"
           placeholder="#FF0000"
+          maxLength={7}
           className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-amber-400"
         />
         <button
@@ -62,18 +90,9 @@ const EditSpace = () => {
         >
           Apply Color
         </button>
-
+        <button  className='bg-amber-500 w-30 h-10 rounded-full' onClick={texturePopup}>Add textures</button>
       </div>
-      <button className=' bg-amber-500 
-            hover:bg-amber-600 
-            text-white 
-            font-medium 
-            py-2 
-            px-4 
-            rounded 
-            shadow 
-            transition-all 
-            duration-300'>select textures</button>
+      
     </div>
   )
 }
--- src/components/ThreeSpace.tsx
+++ src/components/ThreeSpace.tsx
@@ -1,4 +1,5 @@
 import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 import { useEffect } from 'react'
 import { Mesh, MeshStandardMaterial } from 'three'
 import * as THREE from 'three'
@@ -6,17 +7,25 @@ import * as THREE from 'three'
 function TshirtModel({ color }: { color: string }) {
   const { scene } = useGLTF('/tshirt.glb')
 
-  const [normaltexture,diffusetexture] = useTexture(['/textures/normaltshirt.jpg','/textures/diffusetshirt.jpg'])
+  const [normalTexture] = useTexture(['/textures/texture1.jpg'])
+
+  // useFrame(() => {
+  //   normalTexture.offset.x+=0.001
+  // })
   useEffect(() => {
 
-    diffusetexture.wrapS = diffusetexture.wrapT = THREE.RepeatWrapping
-    normaltexture.wrapS = normaltexture.wrapT = THREE.RepeatWrapping
-    diffusetexture.repeat.set(0.30,0.30)
-    normaltexture.repeat.set(0.30,0.30)
+    normalTexture.wrapS = normalTexture.wrapT = THREE.RepeatWrapping
+    normalTexture.repeat.set(2,2)
+    normalTexture.minFilter = THREE.LinearMipMapLinearFilter
+    normalTexture.magFilter = THREE.LinearFilter
+    normalTexture.offset.set(50,50)
+    normalTexture.needsUpdate = true
+
+
 
     scene.traverse((child) => {
       if (child instanceof Mesh && child.name === 'T-Shirt_Material_0') {
-        child.material = new MeshStandardMaterial({color:color,map:diffusetexture})
+        child.material = new MeshStandardMaterial({color:color,map:normalTexture,roughness:0.65})
         child.material.needsUpdate = true
         
       }
@@ -33,7 +42,7 @@ useGLTF.preload('/models/tshirt.glb')
 const ThreeSpace = ({color}:{color:string}) => {
   return (
     <>
-      <ambientLight intensity={0.6} />
+      <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, -1]} intensity={0.5} />
       <TshirtModel color={color} />
       <OrbitControls />
